5b893923659442919127c864ea22c421a360f62f
bcline01 <brookecline.bc@gmail.com>
2024-08-09 21:27:21 -0400

    worked in weatherService

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,14 +2,57 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
-
+interface Coordinates {
+  lat: string;
+  lon: string;
+}
 // TODO: Define a class for the Weather object
+class Weather {
+  temperature: number;
+  wind: string;
+  humidity: number;
+  description: string;
+  icon: string;
+  date: string;
 
+  constructor(temperature: number, wind: string, humidity: number, description: string, icon: string, date:string) {
+    this.temperature: temperature;
+    this.wind: wind;
+    this.humidity: humidity;
+    this.description: description;
+    this.icon: icon;
+    this.date: date;
+  }
+}
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+   baseURL: string;
+   apiKey: string;
+   cityName: string;
+
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+    this.apiKey = process.env.API_KEY || '';
+    this.cityName = '';
+  }
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string): Promise<Coordinates> {
+    const response = await fetch(`${this.baseURL}/geo/1.0/direct?q=${query}&limit=1&appid=${this.apiKey}`);
+    const data = await response.json();
+  
+    if (data.length === 0) {
+      throw new Error('No location found');
+    }
+  
+    const locationData = data[0];
+  
+    return {
+      lat: locationData.lat,
+      lon: locationData.lon,
+    };
+  }
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
   // TODO: Create buildGeocodeQuery method
